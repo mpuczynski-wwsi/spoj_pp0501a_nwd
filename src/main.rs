5ff137806f29cf61540de5bd3d83a0b713791f36
Miroslaw Puczynski <miroslaw@puczynski.org>
2020-04-14 17:37:04 +0200

    finish exercise

--- src/main.rs
+++ src/main.rs
@@ -41,5 +41,17 @@ fn nwd(a: u32, b:u32) -> u32 {
 }
 
 fn main() {
-    println!("{}", nwd(12, 48));
+    let stdin = std::io::stdin();
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer).expect("err");
+
+    let t:u16 = buffer.trim().parse().unwrap();
+    for _ in 0..t {
+        buffer.clear();
+        stdin.read_line(&mut buffer).expect("err");
+        let v: Vec<_> = buffer.split(' ').map(|el| el.trim().parse::<u32>().unwrap()).collect();
+        let (a, b) = (v[0], v[1]);
+        println!("{}", nwd(a, b));
+
+    }
 }
